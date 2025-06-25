40a53b727ea852a3e0b8bd33e96c61952014f976
GaelGil <31393159+GaelGil@users.noreply.github.com>
2025-06-25 14:52:04 -0700

    updating frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/LogIn";
 import "./css/App.css";
+import Navigation from "./components/NavBar";
 
 function App() {
   return (
     <>
+      <Navigation />
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,27 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Home
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link
+              as={Link}
+              to="/https://github.com/GaelGil/algorithm-visualizer"
+            >
+              About
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
