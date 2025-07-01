59ea89d9feb502c20628249b823798d519b06347
Gael <gaelgilbello27@gmail.com>
2025-06-30 21:43:23 -0700

    more backend stuff

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -14,9 +14,9 @@ def signup():
     password = data.get('password')
 
     if not username or not password:
-        return jsonify({'error': "username and password required"})
+        return jsonify({'error': "username and password required"}), 400
     if User.query.filter_by(username=username).first():
-        return jsonify({'error': 'username taken'})
+        return jsonify({'error': 'username taken'}), 409
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
     new_user = User(username=username, email=email, password=hashed_password)
     db.session.add(new_user)
@@ -25,10 +25,28 @@ def signup():
         db.session.commit()
     except IntegrityError:
         db.session.rollback()
-        return jsonify({'error': "database error"})
-    return jsonify({'msg': 'signup succesful'})
+        return jsonify({'error': "database error"}), 500
+    return jsonify({'msg': 'signup succesful'}), 201
 
 @auth.route('/login', methods=['POST'])
 def login():
-    return jsonify({'msg': 'login succesful'})
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    if not username or not password:
+        return jsonify({'error': "username and password required"}), 400
+    
+    user = User.query.filter_by(username=username).first()
+    if not user or not bcrypt.check_password_hash(user.password, password)
+        return jsonify({'error': 'invalid username or password'}), 401
+    
+    access_token = create_access_token(identity=user.id)
+    return jsonify({
+        'message': 'Login successful',
+        'token': access_token,
+        'user': {
+            'id': user.id,
+            'username': user.username
+        }
+    }), 200
 
