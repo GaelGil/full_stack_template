a1444cd017e7f916531b77e30f128012c4c9bc74
Gael <gaelgilbello27@gmail.com>
2025-07-03 13:47:44 -0700

    updating stuff

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -1,10 +1,26 @@
 from flask import Blueprint, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-
+from app.models.user import User
 users = Blueprint('users', __name__, url_prefix='/users')
 
-@users.route('/me', methods=['GET'])
+@users.route('/profile/<int:user_id>', methods=['GET'])
 @jwt_required()
-def get_profile():
-    user_id = get_jwt_identity()
-    return jsonify({'msg': f'Your profile, user {user_id}'})
+def get_user_friends(user_id):
+    current_user_id = get_jwt_identity()
+    if user_id != int(current_user_id):   # convert to int for correct comparison
+        return jsonify({'msg': 'unauthorized access'}), 403
+    
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'msg': 'User not found'}), 404
+    
+
+    friends_list = [
+        {
+            'id': friend.id,
+            'username': friend.username,
+        }
+        for friend in user.friends
+    ]
+
+    return jsonify({'friends': friends_list}), 200
