03928ea267579defa820afe0576bfed6166310c6
Gael <gaelgilbello27@gmail.com>
2025-07-07 22:51:09 -0700

    updated more api functions

--- frontend/src/api/posts.ts
+++ frontend/src/api/posts.ts
@@ -12,3 +12,55 @@ export const getUserPosts = async (userId: number | string, token: string) => {
   const data = await res.json();
   return data.post;
 };
+
+export const getPost = async (userId: number | string, token: string) => {
+  const res = await fetch(`${BASE_URL}/posts/${userId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!res.ok) {
+    return new Error("Error");
+  }
+  const data = await res.json();
+  return data.post;
+};
+
+export const searchPosts = async (userId: number | string, token: string) => {
+  const res = await fetch(`${BASE_URL}/posts/${userId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!res.ok) {
+    return new Error("Error");
+  }
+  const data = await res.json();
+  return data.post;
+};
+
+export const deletePost = async (userId: number | string, token: string) => {
+  const res = await fetch(`${BASE_URL}/posts/${userId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!res.ok) {
+    return new Error("Error");
+  }
+  const data = await res.json();
+  return data.post;
+};
+
+export const editPost = async (userId: number | string, token: string) => {
+  const res = await fetch(`${BASE_URL}/posts/${userId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!res.ok) {
+    return new Error("Error");
+  }
+  const data = await res.json();
+  return data.post;
+};
--- frontend/src/api/users.ts
+++ frontend/src/api/users.ts
@@ -13,6 +13,19 @@ export const getUserProfile = async (userId: string, token: string) => {
   return data;
 };
 
+export const getUser = async (userId: string, token: string) => {
+  const res = await fetch(`${BASE_URL}/profile/${userId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!res.ok) {
+    return new Error("Error");
+  }
+  const data = await res.json();
+  return data;
+};
+
 export const getUserFriends = async (userId: string, token: string) => {
   const res = await fetch(`${BASE_URL}/friends/${userId}`, {
     headers: {
