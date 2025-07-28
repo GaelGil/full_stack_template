be2949779761819983cc7ca7b4c051410cd019af
Gael <gaelgilbello27@gmail.com>
2025-07-28 12:18:47 -0700

    fixing issues

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,6 +4,7 @@ from app.extensions import db, bcrypt, jwt, migrate, socketio, blacklist
 from app.routes import register_routes
 from flask_cors import CORS
 
+
 @jwt.token_in_blocklist_loader
 def check_if_token_revoked(jwt_header, jwt_payload):
     jti = jwt_payload["jti"]
@@ -18,11 +19,14 @@ def create_app():
     socketio.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
-    CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
+    CORS(
+        app,
+        resources={r"/*": {"origins": "http://localhost:5173"}},
+        supports_credentials=True,
+    )
 
     with app.app_context():
-        from app.user.models import user
-
+        from app.user import models as user_models
 
     register_routes(app)
     return app
