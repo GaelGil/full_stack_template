574acfd5902b7848df9c58095e192783efe6cd95
Kevin Gil <kevin_gil@live.com>
2025-07-31 21:42:30 -0700

    add agent scaffolding

--- backend/app/models/post.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text, nullable=False)
-    timestamp = db.Column(db.DateTime, server_default=db.func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
