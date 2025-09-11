38fce5e48b8c11c730a46cef19fa4d28b984dec5
Gael <gaelgilbello27@gmail.com>
2025-09-10 20:50:27 -0700

    updating stuff

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "fallback-uri")
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_COOKIE_NAME = "session"
     SESSION_COOKIE_SAMESITE = "Lax"
